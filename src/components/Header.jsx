cc78192644bea1424a012511332b30971a3d449f
ayushkumar21161 <ayush@homeasy.io>
2026-02-25 12:58:10 +0530

    Updated project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaTractor } from "react-icons/fa6";
 import logo from "../assets/Logo.png"; 
 export default function Header() {
   return (
@@ -27,7 +28,7 @@ export default function Header() {
             className="text-text-main dark:text-black text-sm font-medium leading-normal hover:text-primary transition-colors"
             href="/About"
           >
-            About
+            About Us
           </a>
           <a
             className="text-text-main dark:text-black text-sm font-medium leading-normal hover:text-primary transition-colors"
@@ -39,23 +40,23 @@ export default function Header() {
             className="text-text-main dark:text-black text-sm font-medium leading-normal hover:text-primary transition-colors"
             href="/contact"
           >
-            Contact
+            Contact us
           </a>
           <a
             className="text-text-main dark:text-black text-sm font-medium leading-normal hover:text-primary transition-colors"
             href="/Showcase"
           >
-            showcase
+            News & Announcements
           </a>
        
         </div>
        <a href="/get-quote">
          <button className="px-6 py-2 rounded-full font-semisolid text-black bg-green-400 shadow-md hover:scale-105 transition-transform duration-200">
-          <span className="truncate">Get a Quote</span>
+          <span className="truncate">Spawn Booking</span>
         </button>
        </a>
       </div>
-      <div classn="md:hidden">
+      <div className="md:hidden">
         <button className="text-text-main dark:text-white"></button>
       </div>
     </header>
